27eca761f6ece6e9cb1ee0b5fb63107aa7c74a71
lurgoyf82 <lurgoyf82@hotmail.com>
2025-04-25 21:39:13 +0200

    Update Docker configurations for microservices
    Refactor `docker-compose.yml` files to:
    - Set specific context paths for each service.
    - Reference service-specific environment files.
    - Comment out the `net-api-gateway-frontend` network.
    - Simplify `up-all.sh` script for better readability.
    - Ensure consistent `container_name` entries for clarity.
    
    These changes improve organization and clarity in the Docker setup.

--- up-all.sh
+++ up-all.sh
@@ -1,15 +1,1 @@
-#!/usr/bin/env bash
-#  -f frontend/docker-compose.yml \
-set -e
-
-docker compose \
-  -f docker-compose.yml \
-  -f api-gateway/docker-compose.yml \
-  -f analytics-service/docker-compose.yml \
-  -f auth-service/docker-compose.yml \
-  -f catalog-service/docker-compose.yml \
-  -f notification-service/docker-compose.yml \
-  -f order-service/docker-compose.yml \
-  -f payment-service/docker-compose.yml \
-  -f user-service/docker-compose.yml \
-  up -d
+docker compose -f docker-compose.yml -f api-gateway/docker-compose.yml -f analytics-service/docker-compose.yml -f auth-service/docker-compose.yml -f catalog-service/docker-compose.yml -f notification-service/docker-compose.yml -f order-service/docker-compose.yml -f payment-service/docker-compose.yml -f user-service/docker-compose.yml up -d

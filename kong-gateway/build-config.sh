69869301ca819e514bca049344844941b43e26cd
lurgoyf82 <lurgoyf82@hotmail.com>
2025-05-22 10:15:35 +0200

    Update configuration scripts and service definitions
    - Enhanced `build-config.sh` with error handling and directory creation for declarative config.
    - Removed `_format_version` and `services` sections from multiple service YAML files while retaining and reformatting the `methods` array.
    - Added a new `base-config.yml` file with complete configurations for auth, jwt, and a default redirect service, including plugin settings.

--- kong-gateway/build-config.sh
+++ kong-gateway/build-config.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-cat /tmp/conf.d/*.yml > /tmp/kong.yml
+set -e
+mkdir -p /usr/local/kong/declarative
+cat /tmp/base-config.yml > /usr/local/kong/declarative/kong.yml
+#cat /tmp/conf.d/*.yml >> /usr/local/kong/declarative/kong.yml

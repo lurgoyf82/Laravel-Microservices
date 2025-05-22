8bbab3a479b2b339f2a58a53c670eebbe84a903e
lurgoyf82 <lurgoyf82@hotmail.com>
2025-05-22 11:02:40 +0200

    Update Kong configuration and Docker setup
    - Updated Dockerfile to use `kong:3.6` and added configuration files.
    - Introduced a build script (`build-config.sh`) to handle configuration.
    - Removed a comment from `base-config.yml` and modified service structure.
    - Adjusted `build-config.sh` to concatenate configuration files.
    - Removed volume mapping for `kong.yml` in `docker-compose.yml`.

--- kong-gateway/build-config.sh
+++ kong-gateway/build-config.sh
@@ -2,4 +2,4 @@
 set -e
 mkdir -p /usr/local/kong/declarative
 cat /tmp/base-config.yml > /usr/local/kong/declarative/kong.yml
-#cat /tmp/conf.d/*.yml >> /usr/local/kong/declarative/kong.yml
+cat /tmp/conf.d/*.yml >> /usr/local/kong/declarative/kong.yml
